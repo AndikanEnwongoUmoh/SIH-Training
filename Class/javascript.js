bfc11f0784e4bf591a3c1c657c65facd3665b5fb
AndikanEnwongoUmoh <AndikanEnwongoUmoh@gmail.com>
2024-02-11 14:14:33 +0100

    landing page

---
+++ Class/javascript.js
@@ -0,0 +1,75 @@
+// let age = 35;
+// console.log(age);
+// alert(age);
+// age = 100;
+// console.log(age);
+
+// string
+// let firstName = "Joyce";
+// console.log(firstName);
+
+// number
+// let age = 36;
+// console.log(age);
+
+// boolean
+// let isMarried = true;
+// console.log(isMarried);
+
+// undefined
+// let name;
+// console.log(name);
+
+// null
+// let password = null;
+// console.log(password);
+
+// activity
+// let firstName = "Andikan";
+// console.log(firstName);
+
+// let secondName = "Umoh";
+// console.log(secondName);
+
+// string concatenation
+// console.log("My name is " + firstName + " " + secondName)
+
+// string template literals
+// let age = 60;
+// let fullName = "Godswill Akpabio";
+
+// console.log(`The senate President is from my state, his name is ${fullName} and he is ${age} years old`);
+
+
+//classwork
+// let firstName = "Andikan";
+// let lastName = "Umoh";
+// let state = "Akwa-ibom";
+// let localGovt = "Uyo";
+// let gender = "Male"
+// let phoneNo = "09056604085";
+// let isMarried = false;
+
+// console.log("my name is " + firstName + " " + lastName + "," + " " + "I am from " + localGovt + " " + "in " + state + "." + " " + "My relationship status is " + isMarried + " " + "and my phone number is " + phoneNo)
+
+// console.log(`my name is ${firstName} ${lastName}, I am from ${localGovt} in ${state}. My relationship status is ${isMarried} and my phone number is ${phoneNo}`)
+
+// prompt
+// let number = 5;
+// console.log(String(number));
+
+// let num1 = Number(prompt("Enter first number"));
+// let num2 = Number(prompt("Enter second number"));
+// let num3 = Number(prompt("Enter third number"));
+
+
+// let sum = num1 + num2 + num3;
+// console.log("the sum is " + sum);
+
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter second number"));
+let num3 = Number(prompt("Enter third number"));
+
+
+let avg = (num1 + num2 + num3)/3;
+console.log("the average is " + avg);
